383bb96d1eda42d3e7203a7e580bcb213a02c4bc
NguyenGiangDev <nguyengiangdevw@gmail.com>
2025-11-27 16:01:12 +0700

    update project

---
+++ trip-service/config/db.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect("mongodb://172.25.101.47:27017/trip-service", {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+         console.log('MongoDB connected...');
+    }
+     catch (error) {
+        console.error('Error connecting to MongoDB:', error);
+        process.exit(1); // dừng server nếu không kết nối được DB
+     }
+}
+
+module.exports = connectDB;
